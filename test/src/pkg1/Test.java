5bb8371751c4f26251dfb0b361926c3e271e0722
hadoop <hadoop@shaleenPC>
2014-03-12 12:47:40 +0530

    First change

--- test/src/pkg1/Test.java
+++ test/src/pkg1/Test.java
@@ -7,6 +7,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		System.out.print("Hello Everyone\n");
+		System.out.print("Hello Everyone\n");System.out.print("Hello Everyone\n");
 
 	}
 
